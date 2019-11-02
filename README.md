beda01b8377647f3905808db26a60c05dc2c6cef
pfg <omegariisthebest@gmail.com>
2019-11-02 10:46:13 -0700

    WIP watchable lists, missing updates

--- README.md
+++ README.md
@@ -1,5 +1,76 @@
 Data-driven dom updates
 
+# Demo
+
+```
+let data = watchable({count: 5});
+
+document.body.appendChild((
+<div>
+	Count: {watch([data.count], () => data.count.ref)}{" "}
+	<button>Increase Count</button>
+</div>
+).node);
+```
+
 # Why domframework instead of ...
 
 domframework emphasizes performance. renders only happen when a component is created, then only the necessary parts of the component are updated.
+
+-   Best practices include performance
+-   ( in react the "best practice" is to do everything as inefficiently as possible until performance starts getting impacted )
+-   Typescript everywhere, even in templates
+-   ( in angular, vue, lit-html, svelte they don't)
+-   No diffing or "rerendering"
+-   ( in react, lit-html to update some part of the page, you return an object which gets diffed with the real page to find what needs changing (it's more complicated than that))
+-   Embeddable into existing pages and frameworks
+-   ( every other framework can too, this isn't really very special)
+
+```
+domframework
+
+let data = watchable({a: 1, b: 2});
+
+document.body.appendChild((
+<div>
+	<input type="number" value={data} />
+	<p>{data.$a} {data.$b}, {watch([data.a, data.b], () => data.a + data.b)}</p>
+</div>
+).node);
+
+// ^ lots of that can be minimized by building a language (like svelte does) but doing this removes good type checking
+
+---
+
+react (with hooks)
+
+import React, { useState } from 'react';
+
+export default () => {
+  const [a, setA] = useState(1);
+  const [b, setB] = useState(2);
+
+  return (
+    <div>
+      <input type="number" value={a} onChange={e => setA(e.currentTarget.value)}/>
+      <input type="number" value={b} onChange={e => setB(e.currentTarget.value)}/>
+      {null/* this isn't quite correct because it doesn't handle cases when the number field contains strings */}
+      <p>{a} + {b} = {a + b}</p>
+    </div>
+  );
+};
+
+---
+
+svelte:
+
+<script>
+	let a = 1;
+	let b = 2;
+</script>
+
+<input type="number" bind:value={a}>
+<input type="number" bind:value={b}>
+
+<p>{a} + {b} = {a + b}</p>
+```
--- src/index.tsx
+++ src/index.tsx
@@ -47,23 +47,25 @@ function isWatchable<T>(v: T | Watchable<T>): v is Watchable<T> {
 
 function createComponent(
     c: ComponentModel,
-    parent: ChildNode
+    insert: (node: Node) => void
 ): { finalNode: Node; removeSelf: () => void } {
     if (typeof c === "string") {
         let textNode = document.createTextNode(c);
-        parent.appendChild(textNode);
+        insert(textNode);
         return { finalNode: textNode, removeSelf: () => textNode.remove() };
     }
     if (isWatchable(c)) {
         let finalNode = document.createTextNode(""); // nodes are inserted before this node
-        parent.appendChild(finalNode);
+        insert(finalNode);
         let removalFunctions: (() => void)[] = [];
         let updateValue = (model: ComponentModel) => {
             // clear outdated nodes
             removalFunctions.map(rf => rf());
             removalFunctions = [];
             // create new nodes
-            let { removeSelf } = createComponent(model, parent);
+            let { removeSelf } = createComponent(model, node =>
+                finalNode.parentNode!.insertBefore(node, finalNode)
+            );
             removalFunctions.push(removeSelf);
         };
         let unwatch = c[watchable_watch](updateValue);
@@ -78,7 +80,7 @@ function createComponent(
             }
         };
     }
-    parent.appendChild(c.node);
+    insert(c.node);
     return {
         finalNode: c.node,
         removeSelf: () => {
@@ -118,7 +120,9 @@ const d = (
         });
     if (children)
         children.map(c => {
-            removalHandlers.push(createComponent(c, element).removeSelf);
+            removalHandlers.push(
+                createComponent(c, node => element.appendChild(node)).removeSelf
+            );
         });
     return {
         node: element,
@@ -132,6 +136,7 @@ const d = (
 // type RealOrWatchable<T> = T | Watchable<T>;
 
 const watchable_setup = Symbol("setup");
+const watchable_emit = Symbol("emit");
 const watchable_cleanup = Symbol("cleanup");
 const watchable_cleanupfns = Symbol("cleanupfns");
 const watchable_data = Symbol("data");
@@ -154,6 +159,9 @@ abstract class WatchableBase<T> implements Watchable<T> {
             console.log("done", this[watchable_watchers], watcher);
         };
     }
+    [watchable_emit](value: T) {
+        this[watchable_watchers].map(w => w(value));
+    }
     abstract [watchable_value](): T;
     abstract [watchable_setup](): void;
     abstract [watchable_cleanup](): void;
@@ -172,14 +180,12 @@ class WatchableDependencyList<T> extends WatchableBase<T> {
         return this[watchable_cb]();
     }
     [watchable_setup]() {
-        this[watchable_data].map(dataToWatch => {
+        this[watchable_data].forEach(dataToWatch => {
             this[watchable_cleanupfns].push(
                 dataToWatch[watchable_watch](() => {
                     // when any data changes
                     let valueToReturn = this[watchable_value](); // get our own value
-                    this[watchable_watchers].map(watcher =>
-                        watcher(valueToReturn)
-                    ); // emit our value
+                    this[watchable_emit](valueToReturn);
                 })
             );
         });
@@ -203,7 +209,7 @@ class WatchableRef<T> extends WatchableBase<void> {
     }
     set ref(nv: T) {
         this[watchable_ref] = nv;
-        this[watchable_watchers].map(watcher => watcher());
+        this[watchable_emit]();
     }
     [watchable_value]() {
         return undefined; // watchable refs actually have no value
@@ -432,6 +438,267 @@ document.body.appendChild(
         <button onclick={() => console.log(watchableCount, contentIsShowing)}>
             log
         </button>
+    ).node
+);
+
+type AddHandler<T extends Watchable<any>> = (
+    item: T, // from {prev, next}
+    to: { prevSymbol?: symbol; thisSymbol: symbol; nextSymbol?: symbol }
+) => void;
+type UpdateHandler<T extends Watchable<any>> = (
+    item: T,
+    from: { prevSymbol?: symbol; thisSymbol: symbol; nextSymbol?: symbol },
+    to: { prevSymbol?: symbol; thisSymbol: symbol; nextSymbol?: symbol }
+) => void;
+type RemoveHandler<T extends Watchable<any>> = (
+    item: T,
+    from: { prevSymbol?: symbol; thisSymbol: symbol; nextSymbol?: symbol }
+) => void;
+
+class WatchableArray<T extends Watchable<any>> extends WatchableBase<void> {
+    private __items: { value: T; remove: () => void; symbol: symbol }[] = [];
+    private __addHandlers: (AddHandler<T>)[] = [];
+    private __updateHandlers: (UpdateHandler<T>)[] = [];
+    private __removeHandlers: (RemoveHandler<T>)[] = [];
+    [watchable_value](): void {
+        return undefined; // watchable arrays actually have no value
+    }
+    [watchable_setup](): void {
+        // throw new Error("Method not implemented.");
+    }
+    [watchable_cleanup](): void {
+        // throw new Error("Method not implemented.");
+    }
+    onAdd(handler: AddHandler<T>) {
+        this.__addHandlers.push(handler);
+        return () =>
+            (this.__addHandlers = this.__addHandlers.filter(
+                h => h !== handler
+            ));
+    }
+    onUpdate(handler: UpdateHandler<T>) {
+        this.__updateHandlers.push(handler);
+        return () =>
+            (this.__updateHandlers = this.__updateHandlers.filter(
+                h => h !== handler
+            ));
+    }
+    onRemove(handler: RemoveHandler<T>) {
+        this.__removeHandlers.push(handler);
+        return () =>
+            (this.__removeHandlers = this.__removeHandlers.filter(
+                h => h !== handler
+            ));
+    }
+    push(newItem: T) {
+        let newItemValue = {
+            value: newItem,
+            remove: newItem[watchable_watch](() => {
+                this[watchable_emit]();
+            }),
+            symbol: Symbol("item")
+        };
+        let prevItem = this.__items[this.__items.length - 1];
+        this.__items.push(newItemValue);
+        this.__addHandlers.forEach(h =>
+            h(newItemValue.value, {
+                prevSymbol: prevItem ? prevItem.symbol : undefined,
+                thisSymbol: newItemValue.symbol,
+                nextSymbol: undefined
+            })
+        );
+        this[watchable_emit]();
+    }
+    forEach(cb: (it: T, i: number, symbol: symbol) => void) {
+        this.__items.forEach((item, i) => cb(item.value, i, item.symbol));
+    }
+    get ref() {
+        return this.__items.map(it => ({ symbol: it.symbol, value: it.value }));
+    }
+    set ref(items: ({ symbol: symbol; value: T })[]) {
+        let existingItems: {
+            [key: /*symbol*/ string]: {
+                value: T;
+                remove: () => void;
+                index: number;
+            };
+        } = {};
+        this.__items.forEach(
+            (item, index) =>
+                (existingItems[(item.symbol as unknown) as string] = {
+                    ...item,
+                    index
+                })
+        );
+        let newItems: {
+            [key: /*symbol*/ string]: {
+                value: T;
+                index: number;
+            };
+        } = {};
+        items.forEach((item, index) => {
+            // new items need to be created
+            // existing items need to be updated
+            newItems[(item.symbol as unknown) as string] = {
+                ...item,
+                index
+            };
+        });
+    }
+    // diffing, finds out which items are new, modified, moved, ...
+    overwrite(key: (obj: T) => string, newArray: T[]) {
+        // one of the few things that does diffing
+        let existingItems: {
+            [key: string]: { value: T; remove: () => void; index: number };
+        } = {};
+        this.__items.forEach(
+            (item, index) =>
+                (existingItems[key(item.value)] = { ...item, index })
+        );
+        let newItems: {
+            [key: string]: { value: T; index: number };
+        } = {};
+        newArray.forEach(
+            (item, index) => (newItems[key(item)] = { value: item, index })
+        );
+        // diff and find:: items to remove, items to add, items to move
+        // set this.__items to the new list
+        // ---- usage ----
+        // overwrite the entire list without deleting and recreating items
+        // for example: array.overwrite(i => i.key, array.filter(i => i != itemToDelete))
+    }
+    /*item_add :: add item, add watcher to run our own emit*/
+    /*item_remove :: remove item, remove watcher*/
+    /*on anymodify,add,move,remove*/
+}
+
+function ArrayRender<T extends Watchable<any>>(props: {
+    array: WatchableArray<T>;
+    children: (e: T, s: symbol) => JSX.Element;
+    // index::
+    // what to do about index?
+    // maybe make it a watchable number as part of T
+    // index is definitely necessary but we shouldn't have to rerender everything just because index
+}): JSX.Element {
+    let result = <div></div>; // we don't have intrinsics yet but they are possible to add
+    let removalHandlers = [];
+    let nodeInformation: {
+        [key: string]: { value: T; removeSelf: () => void; finalNode: Node };
+    } = {};
+    // add pre-existing items ::
+    props.array.forEach((item, index, symbol) => {
+        let { finalNode, removeSelf } = createComponent(
+            props.children(item, symbol),
+            node => result.node.appendChild(node)
+        );
+        nodeInformation[(symbol as unknown) as string] = {
+            removeSelf,
+            finalNode,
+            value: item
+        };
+    });
+    // handle changes
+    removalHandlers.push(
+        props.array.onAdd((item, { prevSymbol, thisSymbol, nextSymbol }) => {
+            let infoBefore = nodeInformation[(prevSymbol as unknown) as string];
+            let nodeInsertFn: (node: Node) => void;
+            if (infoBefore) {
+                let next = infoBefore.finalNode.nextSibling;
+                if (next) {
+                    nodeInsertFn = node => result.node.insertBefore(node, next);
+                } else {
+                    nodeInsertFn = node => result.node.appendChild(node);
+                }
+            } else {
+                nodeInsertFn = node =>
+                    ((result.node as unknown) as ParentNode).prepend(node);
+            }
+            let { finalNode, removeSelf } = createComponent(
+                props.children(item, thisSymbol),
+                nodeInsertFn // !!!!!!!!!!! maybe instead of all this work to define nodeInsertFn here, we could have nodeBefore as a parameter to createComponent?
+            );
+            nodeInformation[(thisSymbol as unknown) as string] = {
+                removeSelf,
+                finalNode,
+                value: item
+            };
+        })
+    );
+    // todo onupdate
+    // if prev.thisSymbol !== curr.thisSymbol
+    //// ??? magic (not sure if it's possible to move a node model unless it contains all the elements)
+    //// for now, probably just delete and recreate the node
+    removalHandlers.push(
+        props.array.onRemove((item, { prevSymbol, thisSymbol, nextSymbol }) => {
+            let nodeInfo = nodeInformation[(thisSymbol as unknown) as string];
+            nodeInfo.removeSelf();
+            delete nodeInformation[(thisSymbol as unknown) as string];
+        })
+    );
+    return result;
+}
+
+function makeWatchablesFromBuiltins() {}
+
+let watchablearray = new WatchableArray<WatchableRef<string>>();
+
+/*
+<ArrayRender array={watchablearray}>
+    {element => (
+        <li>
+            <input
+                value={element.$ref}
+                onInput={e =>
+                    (element.ref = e.currentTarget.value)
+                }
+            />
+        </li>
+    )}
+</ArrayRender>
+*/
+
+let updateCount = 0;
+
+document.body.appendChild(
+    (
+        <div>
+            <div>
+                Array Updates:{" "}
+                {watch([watchablearray], () => "" + updateCount++)}
+            </div>
+            <ul>
+                {ArrayRender({
+                    array: watchablearray,
+                    children: (element, symbol) => (
+                        <li>
+                            <input
+                                value={element.$ref}
+                                oninput={e =>
+                                    (element.ref = (e.currentTarget! as any).value)
+                                }
+                            />
+                            <span>{element.$ref}</span>
+                            <button
+                                onclick={() => {
+                                    watchablearray.ref = watchablearray.ref.filter(
+                                        e => e.symbol !== symbol
+                                    );
+                                }}
+                            >
+                                x
+                            </button>
+                        </li>
+                    )
+                })}
+            </ul>
+            <button
+                onclick={() =>
+                    watchablearray.push(new WatchableRef("new element"))
+                }
+            >
+                +Elem
+            </button>
+        </div>
     ).node
 );
 
