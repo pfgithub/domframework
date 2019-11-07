235858fc25c4f9b0d6a9781fef3d036a75ba1291
pfg <omegariisthebest@gmail.com>
2019-11-07 10:58:41 -0800

    Remove comments  from babel plugin

--- src/babelplugin.wip.js
+++ src/babelplugin.wip.js
@@ -1,25 +1,3 @@
-/*demo code:
-return <div>{<p>{key.value.$ref + "-" + value.value.$ref + cb(name.$ref)}</p>}</div>
-*/
-
-// export default function(babel) {
-//   const { types: t } = babel;
-//
-//   return {
-//     name: "ast-transform", // not required
-//     visitor: {
-//       Identifier(path) {
-//         path.node.name = path.node.name
-//           .split("")
-//           .reverse()
-//           .join("");
-//       }
-//     }
-//   };
-// }
-
-// babylon 7 transformer
-
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
@@ -49,7 +27,6 @@ module.exports.default = function({ types: t }) {
                         }
                     }
                 });
-                console.log("FOUND WATCHABLES:", watchables, path);
                 if (watchables.length <= 0) {
                     // nothing to do. don't skip.
                     return;
@@ -62,45 +39,3 @@ module.exports.default = function({ types: t }) {
         }
     };
 };
-
-// export function a(context) {
-//   return {
-//     JSXExpressionContainer(node, ...a) {
-//       console.log(node, ...a);
-//       let expression = node.expression;
-//       let resultWatchables = [];
-//       let MemberExpressionVisitor = {
-// 		  JSXExpressionContainer(path){
-// 			  path.skip();
-// 		  }
-//         MemberExpression(path) {
-// 			path.skip();
-//         }
-//       };
-//       console.log(context);
-//       //node.expression.traverse(MemberExpressionVisitor);
-//       // search inside for MemberExpression .property.name === "$ref",
-//       // add MemberExpression.object to watch
-//       /*context.report({
-//         node,
-//         message: "Do not use template literals",
-//
-//         fix(fixer) {
-//           //console.log(fixer);
-//           // search inside for anything .$ref
-//           // don't search inside another jsxexpressioncontainer,
-//           // each jsxexpressioncontainer gets its own watch statement
-//           //if (node.expressions.length) {
-//           //  // Can't auto-fix template literal with expressions
-//           //  return;
-//           //}
-//
-//           //return [
-//           //  fixer.replaceTextRange([node.start, node.start + 1], '"'),
-//           //  fixer.replaceTextRange([node.end - 1, node.end], '"'),
-//           //];
-//         }
-//       });*/
-//     }
-//   };
-// }
