601ebc4f85b5fcd7b09dc95821eac1fcc67618d6
pfg <omegariisthebest@gmail.com>
2019-11-14 23:45:43 -0800

    Object concept (not working yet)

--- demo/index.tsx
+++ demo/index.tsx
@@ -17,3 +17,40 @@ document.body.appendChild(
         </div>
     ).node
 );
+
+let $obj = {
+    mode: "a",
+    a: 3
+} as { mode: "a"; a: number } | { mode: "b"; b: number };
+
+document.body.appendChild(
+    (
+        <div>
+            {$obj.mode === "a" ? (
+                <div>
+                    <button
+                        onclick={
+                            () =>
+                                ($obj = {
+                                    mode: "b",
+                                    b: 12
+                                }) /* should but doesn't compile to  $obj.$ref = { ... } */
+                        }
+                    >
+                        b mode
+                    </button>
+                    a: {$obj.a}{" "}
+                    <button onclick={() => ++($obj as any).a}>++</button>
+                </div>
+            ) : (
+                <div>
+                    <button onclick={() => ($obj = { mode: "a", a: 3 })}>
+                        a mode
+                    </button>
+                    b: {$obj.b}{" "}
+                    <button onclick={() => --($obj as any).b}>--</button>
+                </div>
+            )}
+        </div>
+    ).node
+);
---
+++ test/babelplugin.js
@@ -0,0 +1,1 @@
+// https://github.com/babel-utils/babel-plugin-tester
