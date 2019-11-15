1f38ca25372571d09420772ca5a197c2c712f834
pfg <omegariisthebest@gmail.com>
2019-11-15 11:28:09 -0800

    Support usage outside of jsxexpressioncontainers

--- src/concepts.js
+++ src/concepts.js
@@ -63,9 +63,73 @@ Object.defineProperty(exports, "__esModule", {
 module.exports.default = function({ types: t }) {
     let $call = v =>
         t.memberExpression(t.identifier("$"), t.identifier("" + v));
+    let wrap = node => t.memberExpression(node, t.identifier("$ref"));
+    let matches = (str, prefix) => str.startsWith(prefix) && str !== prefix;
     return {
         name: "ast-transform",
         visitor: {
+            Identifier: {
+                exit(path) {
+                    let prefix = path.findParent(path => path.isProgram())
+                        .__dmf_prefix;
+                    if (!prefix) return;
+
+                    if (
+                        matches(path.node.name, prefix) &&
+                        (path.parent.type === "VariableDeclarator"
+                            ? path.parent.id !== path.node
+                            : true)
+                    ) {
+                        path.replaceWith(wrap(path.node));
+                        path.skip();
+                    }
+                }
+            },
+            MemberExpression: {
+                exit(path) {
+                    let prefix = path.findParent(path => path.isProgram())
+                        .__dmf_prefix;
+                    if (!prefix) return;
+
+                    if (
+                        path.node.object.type === "MemberExpression" &&
+                        path.node.object.property.name === "$ref"
+                    ) {
+                        path.node.object = path.node.object.object;
+                    }
+                    if (
+                        (path.node.object.type === "Identifier" &&
+                            matches(path.node.object.name, prefix)) ||
+                        (path.node.object.type === "CallExpression" &&
+                            path.node.object.callee.type ===
+                                "MemberExpression" &&
+                            path.node.object.callee.property.type ===
+                                "Identifier" &&
+                            path.node.object.callee.property.name === "$get")
+                    ) {
+                        console.log("got ehre");
+                        let property = path.node.property;
+                        if (property.type === "Identifier") {
+                            //property.name = "test";
+                            path.replaceWith(
+                                wrap(
+                                    t.callExpression(
+                                        // maybe  we can tell it to not check this?
+                                        t.memberExpression(
+                                            path.node.object,
+                                            t.identifier("$get")
+                                        ),
+                                        [t.stringLiteral(property.name)]
+                                    )
+                                )
+                            );
+                            path.skip();
+                            console.log("did replace");
+                        }
+                    }
+                }
+            },
+
             DirectiveLiteral(path) {
                 if (path.node.value.startsWith("dmf prefix ")) {
                     let file = path.findParent(path => path.isProgram());
@@ -76,17 +140,19 @@ module.exports.default = function({ types: t }) {
                 let prefix = path.findParent(path => path.isProgram())
                     .__dmf_prefix;
                 if (!prefix) return;
+
                 if (
                     path.node.id.type === "Identifier" &&
-                    path.node.id.name.startsWith(prefix)
+                    matches(path.node.id.name, prefix)
                 ) {
                     path.node.init = t.callExpression($call`createWatchable`, [
                         path.node.init
                     ]);
                 }
             },
-            JSXExpressionContainer(path) {
-                /*
+            JSXExpressionContainer: {
+                exit(path) {
+                    /*
                 ---
                 it turns out sometimes we need to do things outside of
                 jsxexpresisoncontainer.
@@ -97,104 +163,37 @@ module.exports.default = function({ types: t }) {
                 but because it's not in a jsxexpressioncontainer, that doesn't happen
                 ---
                 */
-                let prefix = path.findParent(path => path.isProgram())
-                    .__dmf_prefix;
-                if (!prefix) return;
-                let watchables = [];
-                path.traverse({
-                    JSXExpressionContainer(path) {
-                        path.skip();
-                    },
-                    exit(path) {
-                        let wrap = node => {
-                            return t.memberExpression(
-                                node,
-                                t.identifier("$ref")
-                            );
-                        };
-                        if (path.node.type === "Identifier") {
-                            if (path.node.name.startsWith(prefix)) {
-                                path.replaceWith(wrap(path.node));
-                                path.skip();
-                            }
-                        }
-                        if (path.node.type === "MemberExpression") {
-                            console.log(
-                                "found memberexpression",
-                                path.node.object
-                            );
+                    let prefix = path.findParent(path => path.isProgram())
+                        .__dmf_prefix;
+                    if (!prefix) return;
+                    let watchables = [];
+                    path.traverse({
+                        // traverse once again and add .ref to watchables
+                        JSXExpressionContainer(path) {
+                            path.skip();
+                        },
+                        Function(path) {
+                            path.skip();
+                        },
+                        MemberExpression(path) {
                             if (
-                                path.node.object.type === "MemberExpression" &&
-                                path.node.object.property.name === "$ref"
+                                path.node.property.type === "Identifier" &&
+                                path.node.property.name === "$ref"
                             ) {
-                                console.log("unwrap", path.node.object);
-                                path.node.object = path.node.object.object;
+                                watchables.push(path.node.object);
                             }
-                            if (
-                                (path.node.object.type === "Identifier" &&
-                                    path.node.object.name.startsWith(prefix)) ||
-                                (path.node.object.type === "CallExpression" &&
-                                    path.node.object.callee.type ===
-                                        "MemberExpression" &&
-                                    path.node.object.callee.property.type ===
-                                        "Identifier" &&
-                                    path.node.object.callee.property.name ===
-                                        "$get")
-                            ) {
-                                console.log("got ehre");
-                                let property = path.node.property;
-                                if (property.type === "Identifier") {
-                                    //property.name = "test";
-                                    path.replaceWith(
-                                        wrap(
-                                            t.callExpression(
-                                                // maybe  we can tell it to not check this?
-                                                t.memberExpression(
-                                                    path.node.object,
-                                                    t.identifier("$get")
-                                                ),
-                                                [t.stringLiteral(property.name)]
-                                            )
-                                        )
-                                    );
-                                    path.skip();
-                                    console.log("did replace");
-                                }
-                                // if(is last) add .ref
-                                // last means last before a fn call
-                                // <>{a.b.toString().c + "d"}</>
-                                // -> a.$get(b).$get(toString).ref().c + "d"
-                            }
-                            // add watch as needed
                         }
-                    }
-                });
-                path.traverse({
-                    // traverse once again and add .ref to watchables
-                    JSXExpressionContainer(path) {
-                        path.skip();
-                    },
-                    Function(path) {
-                        path.skip();
-                    },
-                    MemberExpression(path) {
-                        if (
-                            path.node.property.type === "Identifier" &&
-                            path.node.property.name === "$ref"
-                        ) {
-                            watchables.push(path.node.object);
-                        }
-                    }
-                });
+                    });
 
-                if (watchables.length <= 0) {
-                    // nothing to do. don't skip.
-                    return;
+                    if (watchables.length <= 0) {
+                        // nothing to do. don't skip.
+                        return;
+                    }
+                    path.node.expression = t.callExpression($call`watch`, [
+                        t.arrayExpression(watchables),
+                        t.arrowFunctionExpression([], path.node.expression)
+                    ]);
                 }
-                path.node.expression = t.callExpression($call`watch`, [
-                    t.arrayExpression(watchables),
-                    t.arrowFunctionExpression([], path.node.expression)
-                ]);
             }
         }
     };
