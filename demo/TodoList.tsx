bd13b8badecf3eaf443a1d68a879cf55bc9b5445
pfg <omegariisthebest@gmail.com>
2019-11-30 08:53:49 -0800

    Filter todo list and an issue with excessive rerenders (~~maybe use virtual dom?~~)

--- demo/TodoList.tsx
+++ demo/TodoList.tsx
@@ -10,6 +10,7 @@ type TodoItem = { checked: boolean; contents: string };
 
 function TodoList($list: List<TodoItem>) {
     let $wipItem = "";
+    let $filter = "";
     return (
         <div>
             <h1>Todo List</h1>
@@ -32,24 +33,42 @@ function TodoList($list: List<TodoItem>) {
                             }
                         }}
                     />
+                    <input
+                        type="text"
+                        placeholder="Filter..."
+                        value={$filter}
+                        oninput={e =>
+                            ($filter = (e.currentTarget as any).value)
+                        }
+                    />
                 </li>
                 {ListRender($list, $item => (
-                    <li>
-                        <input
-                            type="checkbox"
-                            checked={$item.checked}
-                            oninput={e =>
-                                ($item.checked = (e.currentTarget as any).checked)
-                            }
-                        />{" "}
-                        <input
-                            type="text"
-                            value={$item.contents}
-                            oninput={e =>
-                                ($item.contents = (e.currentTarget as any).value)
-                            }
-                        />
-                    </li>
+                    <div>
+                        {$item.contents.indexOf($filter) > -1 ? (
+                            // here is an issue!
+                            // we don't need to rerender this!!
+                            // it should be wrapped in something that only
+                            // rerenders it when $item.checked, $item.contents changes, not $filter
+                            <li>
+                                <input
+                                    type="checkbox"
+                                    checked={$item.checked}
+                                    oninput={e =>
+                                        ($item.checked = (e.currentTarget as any).checked)
+                                    }
+                                />{" "}
+                                <input
+                                    type="text"
+                                    value={$item.contents}
+                                    oninput={e =>
+                                        ($item.contents = (e.currentTarget as any).value)
+                                    }
+                                />
+                            </li>
+                        ) : (
+                            <li>does not match filter</li>
+                        )}
+                    </div>
                 ))}
             </ul>
         </div>
@@ -59,3 +78,37 @@ function TodoList($list: List<TodoItem>) {
 let $list: List<TodoItem> = $.list([]);
 document.body.appendChild(TodoList($list || $bind).node);
 document.body.appendChild(TodoList($list || $bind).node);
+
+export function HighlightUpdatesButton() {
+    let $showSection = true;
+    return (
+        <div>
+            {$showSection ? (
+                <div>
+                    <button
+                        onclick={() => {
+                            window.startHighlightUpdates();
+                            $showSection = false;
+                        }}
+                    >
+                        highlight updates
+                    </button>
+                    <button
+                        onclick={() =>
+                            (window.onNodeUpdate = n => console.log(n))
+                        }
+                    >
+                        log updates
+                    </button>
+                    <button onclick={() => (window.onNodeUpdate = () => {})}>
+                        ignore updates
+                    </button>
+                </div>
+            ) : (
+                <div></div>
+            )}
+        </div>
+    );
+}
+
+document.body.appendChild(HighlightUpdatesButton().node);
