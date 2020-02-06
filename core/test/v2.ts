157dfc1b99cbc151c84159021f17aa48d21bc61e
pfg <omegariisthebest@gmail.com>
2020-02-05 18:40:34 -0800

    better test ui (about to get replaced though)

--- core/test/v2.ts
+++ core/test/v2.ts
@@ -1,58 +1,77 @@
-import { WatchableThing, watchable_watch } from "../src/watchable";
+import { WatchableThing } from "../src/watchable";
 import { strict as assert } from "assert";
+
+let interval = 80;
+let spinner = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
 
 async function test(
     name: string,
-    cb: (t: { equal: (a: any, b: any) => void }) => Promise<void>
+    cb: (t: { equal: (a: any, b: any) => void }) => Promise<void>,
 ) {
     let assertCount = cb.toString().split("assert").length - 2;
     let successes = 0;
-    process.stdout.write(" .".repeat(assertCount) + "\r");
+    let total = assertCount;
+    process.stdout.write(name + "\x1b7");
+    let start = new Date().getTime();
+    let spa = 0;
+    let update = () => {
+        process.stdout.write(
+            "\x1b8" +
+                " \x1b[32m✔\x1b[0m".repeat(successes) +
+                " .".repeat(Math.max(total - successes - 1, 0)) +
+                (total - successes - 1 < 0
+                    ? ""
+                    : " " + spinner[(spa = (spa + 1) % spinner.length)]),
+        );
+    };
+    let int = setInterval(() => update(), interval);
+    //defer clearInterval(int)
     await cb({
         equal: (a, b) => {
-            assert.deepStrictEqual(a, b);
-            process.stdout.write(" \x1b[32m✔\x1b[0m");
+            try {
+                assert.deepStrictEqual(a, b);
+            } catch (e) {
+                process.stdout.write("\n");
+                clearInterval(int);
+                throw e;
+            }
             successes++;
-        }
+        },
     });
-    console.log("");
+    clearInterval(int);
+    process.stdout.write("\n");
     assert.deepStrictEqual(successes, assertCount);
 }
 
-test("emitters", async assert => {
-    let eventsThisTick: 1[] = [];
-    let tick = () => {
-        eventsThisTick = [];
-        return new Promise(r => setTimeout(() => r(), 0));
-    };
+let ms = (ms: number) => new Promise(r => setTimeout(r, ms));
 
-    let testThing = new WatchableThing({});
-    // console.log(testThing.$get("a").$ref);
-    testThing.$get("propname")[watchable_watch](() => eventsThisTick.push(1));
-    await tick();
-    assert.equal(eventsThisTick, [1]);
-    // console.log("setting propname. should emit.");
-    testThing.$get("propname").$ref = "new value";
-    await tick();
-    assert.equal(eventsThisTick, [1]);
-    // console.log("setting a. nothing should happen.");
-    testThing.$get("a").$ref = "also changed";
-    await tick();
-    assert.equal(eventsThisTick, []);
-    // console.log("removing propname. should emit.");
-    testThing.$ref = { "propname is no longer a thing": "rip", a: 23 }; // in this case, propname should still exist as an empty
-    await tick();
-    assert.equal(eventsThisTick, [1]);
-    // console.log("adding propname. should emit.");
-    testThing.$ref = { propname: "oh it's back" };
-    await tick();
-    assert.equal(eventsThisTick, [1]);
-
-    assert.equal(testThing.$get("propname").$ref, "oh it's back");
-    assert.equal(
-        (testThing.$get("propname") as any).$get("toString").$ref(),
-        "oh it's back"
-    );
-    // as/**/ */sert.deepStrictEqual(testThing.$ref, { propname: "oh it's back" }); // todo
-    // console.log("testthing is: " + JSON.stringify(testThing));
+test("a", async assert => {
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
 });
