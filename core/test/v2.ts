c7932ab98b2c2aefc0e597dad3599f548be9b673
pfg <omegariisthebest@gmail.com>
2020-02-05 19:02:45 -0800

    update testing ui again (to support parallel tests

--- core/test/v2.ts
+++ core/test/v2.ts
@@ -1,8 +1,57 @@
 import { WatchableThing } from "../src/watchable";
+import * as util from "util";
 import { strict as assert } from "assert";
 
 let interval = 80;
 let spinner = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+
+type LineType = { ref: string } | (() => string) | string;
+function makeUI() {
+    let lines: LineType[] = [];
+    let newlineCount = 0;
+    process.stdout.write("\x1b7");
+    let ui = {
+        addLine(line: LineType) {
+            lines.push(line);
+        },
+        update() {
+            let finalWrite = "";
+            if (newlineCount) finalWrite += "\x1b[" + newlineCount + "A";
+            newlineCount = 0;
+            for (let line of lines) {
+                let lineText: string;
+                if (typeof line === "string") {
+                    lineText = line;
+                } else if (line instanceof Function) {
+                    lineText = line();
+                } else {
+                    lineText = line.ref;
+                }
+                lineText += "\n";
+                let newlineSplit = lineText.split("\n");
+                newlineCount += newlineSplit.length - 1;
+                finalWrite += lineText;
+            }
+            process.stdout.write(finalWrite);
+        },
+    };
+    return ui;
+}
+
+let globalui = makeUI();
+
+console.log = (...msg: any[]) => {
+    globalui.addLine(
+        "[log]: " +
+            msg
+                .map(i =>
+                    typeof i === "string"
+                        ? i
+                        : util.inspect(i, false, null, true),
+                )
+                .join(" "),
+    );
+};
 
 async function test(
     name: string,
@@ -11,18 +60,19 @@ async function test(
     let assertCount = cb.toString().split("assert").length - 2;
     let successes = 0;
     let total = assertCount;
-    process.stdout.write(name + "\x1b7");
+    let lineText = { ref: "..." };
+    globalui.addLine(lineText);
     let start = new Date().getTime();
     let spa = 0;
     let update = () => {
-        process.stdout.write(
-            "\x1b8" +
-                " \x1b[32m✔\x1b[0m".repeat(successes) +
-                " .".repeat(Math.max(total - successes - 1, 0)) +
-                (total - successes - 1 < 0
-                    ? ""
-                    : " " + spinner[(spa = (spa + 1) % spinner.length)]),
-        );
+        lineText.ref =
+            name +
+            " \x1b[32m✔\x1b[0m".repeat(successes) +
+            " .".repeat(Math.max(total - successes - 1, 0)) +
+            (total - successes - 1 < 0
+                ? ""
+                : " " + spinner[(spa = (spa + 1) % spinner.length)]);
+        globalui.update();
     };
     let int = setInterval(() => update(), interval);
     //defer clearInterval(int)
@@ -39,13 +89,78 @@ async function test(
         },
     });
     clearInterval(int);
-    process.stdout.write("\n");
     assert.deepStrictEqual(successes, assertCount);
 }
 
 let ms = (ms: number) => new Promise(r => setTimeout(r, ms));
 
 test("a", async assert => {
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    console.log("hi!");
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+});
+
+test("test two", async assert => {
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+    assert.equal(1, 1);
+    await ms(Math.random() * 1000);
+});
+
+test("test three", async assert => {
+    await ms(Math.random() * 1000);
     assert.equal(1, 1);
     await ms(Math.random() * 1000);
     assert.equal(1, 1);
